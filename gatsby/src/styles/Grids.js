792c634d65ad957c1881f0b29b2993514f87f3ab
Isaac Arnold <isaacarnold@icloud.com>
2021-09-18 17:53:29 +1200

    Typo change

--- gatsby/src/styles/Grids.js
+++ gatsby/src/styles/Grids.js
@@ -4,7 +4,7 @@ export const HomePageGrid = styled.div`
   display: grid;
   grid-gap: 2rem;
   --columns: 2;
-  grid-template-columns: repeat(var(---columns), minmax(auto, 1fr));
+  grid-template-columns: repeat(var(--columns), minmax(auto, 1fr));
   @media (max-width: 800px) {
     --columns: 1;
   }
