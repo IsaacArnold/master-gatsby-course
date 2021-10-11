c9470eb4acca11fb1f32f98238c99225366de3e9
Isaac Arnold <isaacarnold@icloud.com>
2021-10-11 20:51:07 +1300

    Update beers.js

--- gatsby/src/pages/beers.js
+++ gatsby/src/pages/beers.js
@@ -33,7 +33,6 @@ export default function BeersPage({ data }) {
       </h2>
       <BeerGridStyles>
         {data.beers.nodes.map((beer) => {
-          const rating = Math.round(beer.rating.average);
           return (
             <SingleBeerStyles key={beer.id}>
               <img src={beer.image} alt={beer.name} />
